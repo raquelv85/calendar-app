85a96ab46c84ba7219c2b33bb4a4a086ec6875af
RaquelV <raquelv85@gmail.com>
2021-04-07 11:06:55 +0200

    create components and add dateTimePicker

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,79 @@
+import React, {useState} from "react";
+import {Calendar, momentLocalizer} from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/es"
+import { NavBar } from "../ui/NavBar";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import {messages} from "../../helpers/calendar-messages-es"
+import {CalendarEvent} from "./CalendarEvent"
+import {CalendarModal} from "../calendar/CalendarModal"
+
+moment.locale('es')
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños jefe",
+    start: moment().toDate(),
+    end: moment().add( 2,'hours').toDate(),
+    bgColor: "#fafafa",
+    user: {
+      _id: "1234",
+      name: "Raquel"
+    }
+  }
+]
+
+export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem("lastView") || "month")
+
+  const onDoubleClick = (e) => {
+    console.log(e)
+  }
+
+  const onSelectEvent = (e) => {
+    console.log(e)
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e)
+    localStorage.setItem("lastView",e)
+  }
+
+  const eventStyleGetter = (event,start,end,isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+     
+    }
+
+    return {
+      style
+    }
+  }
+
+  return (
+    <div className="calendar-screen">
+      <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        eventPropGetter={eventStyleGetter}
+        components={{event: CalendarEvent}}
+        view={lastView}
+      />
+      <CalendarModal />
+    </div>
+  );
+};
