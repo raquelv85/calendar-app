6a540eacc039c6661a4265dddfa8df9fbeb4c791
RaquelV <raquelv85@gmail.com>
2021-04-13 17:16:28 +0200

    add helper fetchConToken

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,23 +1,40 @@
-const baseUrl = process.env.REACT_APP_API_URL
+const baseUrl = process.env.REACT_APP_API_URL;
 
-const fetchSinToken = ( endpoint, data, method = 'GET' ) => {
-  const url = `${ baseUrl }/${ endpoint }`;
-  
-  if(method === 'GET'){
-    return fetch( url );
-  }else {
+const fetchSinToken = (endpoint, data, method = "GET") => {
+  const url = `${baseUrl}/${endpoint}`;
+
+  if (method === "GET") {
+    return fetch(url);
+  } else {
     return fetch(url, {
       method,
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
-      body: JSON.stringify( data )
-    })
+      body: JSON.stringify(data),
+    });
   }
-}
+};
 
+const fetchConToken = (endpoint, data, method = "GET") => {
+  const url = `${baseUrl}/${endpoint}`;
+  const token = localStorage.getItem("token") || "";
 
+  if (method === "GET") {
+    return fetch(url, {
+      method,
+      headers: { "x-token": token },
+    });
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        "Content-type": "application/json",
+        "x-token": token,
+      },
+      body: JSON.stringify(data),
+    });
+  }
+};
 
-export {
-  fetchSinToken
-}
+export { fetchSinToken, fetchConToken };
