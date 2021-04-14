7e7a15fb7620012d6e99ca2b6f89c2b6368e0efe
RaquelV <raquelv85@gmail.com>
2021-04-14 11:01:24 +0200

    add test to fetchConToken

--- src/test/helpers/fetch.test.js
+++ src/test/helpers/fetch.test.js
@@ -1,6 +1,7 @@
-import { fetchSinToken } from "../../helpers/fetch";
+import { fetchSinToken, fetchConToken } from "../../helpers/fetch";
 
 describe("Pruebas en el helper Fetch", () => {
+  let token = ''
   test("fetchSinToken debe de funcionar", async () => {
     const resp = await fetchSinToken(
       "auth",
@@ -14,5 +15,21 @@ describe("Pruebas en el helper Fetch", () => {
 
     const body = await resp.json();
     expect(body.ok).toBe(true);
+    
+    token = body.token
+   
+  });
+
+  test("fetchConToken debe de funcionar", async () => {
+    localStorage.setItem('token', token );
+    const resp = await fetchConToken(
+      "events/60756cdfd4f6440c5e108a44",
+      {},
+      "DELETE"
+    );
+
+    const body = await resp.json();
+
+    expect( body.msg ).toBe('Evento no existe por ese id');
   });
 });
