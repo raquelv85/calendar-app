8cd91a00d4277bebc56e9d0391a2ae3438559927
RaquelV <raquelv85@gmail.com>
2021-04-14 14:00:18 +0200

    add test to login

--- src/test/actions/auth.test.js
+++ src/test/actions/auth.test.js
@@ -1,10 +1,13 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-
+import Swal from 'sweetalert2'
 import '@testing-library/jest-dom';
 
 import { startLogin } from '../../actions/auth';
-import { types } from '../../types/types'
+import { types } from '../../types/types';
+
+jest.mock('sweetalert2', () => ({fire: jest.fn()}));
+
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -40,5 +43,21 @@ describe('Pruebas en las actions auth', () => {
 
 
   })
+
+  test('startLogin incorrecto', async() => {
+
+    await store.dispatch( startLogin('raquelv85@gmail.com', '12345678') )
+ 
+    let actions = store.getActions();
+ 
+    expect(actions).toEqual([]);
+    expect( Swal.fire ).toHaveBeenCalledWith("Error", "Password incorrecto", "error");
+
+    await store.dispatch( startLogin('raquelv85111@gmail.com', '123456') );
+    actions = store.getActions();
+
+    expect( Swal.fire ).toHaveBeenCalledWith("Error", "El usuario no existe con ese email", "error");
+
+   })
 
 })
